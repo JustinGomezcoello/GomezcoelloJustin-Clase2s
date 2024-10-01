3db0aed37ca79e31b5503355251ebe9ee11ad8d7
laboratorio <laboratorio@UPOAULA10304>
2024-10-01 07:40:50 -0500

    Agregar archivos de proyecto.

---
+++ GomezcoelloJustin-Clase2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using GomezcoelloJustin_Clase2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GomezcoelloJustin_Clase2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GomezcoelloJustin-Clase2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GomezcoelloJustin_Clase2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
